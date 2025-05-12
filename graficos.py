23b5554da52c701b06dc619da517719b6360d54c
brvinfvck <marianvilabeiran@gmail.com>
2025-05-12 18:11:45 +0200

    graficos optimizados

--- graficos.py
+++ graficos.py
@@ -1,21 +1,39 @@
 import numpy as np  
 import librosa
-#faltar mostrar los plot show()
-def calcular_fft(y, sr):
+import matplotlib.pyplot as plt
+
+def fft(y, sr):
     fft_dom = np.fft.rfft(y)
     freqs_vect = np.fft.rfftfreq(len(y), d=1/sr)
     amplitudes = np.abs(fft_dom)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(freqs_vect, amplitudes)
+    plt.title("FFT")
+    plt.xlabel("frecuencia hz")
+    plt.ylabel("amplitud")
+    plt.grid(True)
+    plt.show()
     return fft_dom, freqs_vect, amplitudes
 
 
-def calcular_stft(y):
+def stft(y):
     D = np.abs(librosa.stft(y))
     D_db = librosa.amplitude_to_db(D, ref=np.max)
+
+    plt.figure(figsize=(10, 6))
+    librosa.display.specshow(D_db, x_axis='time', y_axis='log', sr=sr)
+    plt.colorbar(format="%+2.0f dB")
+    plt.title("STFT")
+    plt.show()
+
+
     return D_db
 
-
-
-''' GRAFICOS'''
+# graficos del wav generado
+y, sr = librosa.load('mensaje.wav', sr=None)
+fft(y, sr)
+stft(y)
 
 # fft_dom = np.fft.rfft(y)  # FFT en y
 # freqs_vect = np.fft.rfftfreq(len(y), d=1/sr)  #-> vector con las frecs correspondientes
